7068aab1e5a850486cc8f89bb260eff111f7c822
Nathan <48697917+RinalduzziNathan@users.noreply.github.com>
2020-12-01 11:36:55 +0100

    ajout interface debut + creation perso

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,63 @@
 package sample;
+import javafx.scene.control.TextField;
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 
 public class Controller {
+    @FXML
+    Pane panePerso;
+    @FXML
+    Pane paneJouer;
+    @FXML
+    TextField nomPerso;
+
+    Personnage persoCree;
+
+
+    @FXML
+    public void CreationPerso(){
+        System.out.println(panePerso.getId());
+        panePerso.setVisible(true);
+        paneJouer.setVisible(false);
+    }
+    @FXML
+    public void ButtonJouer(){
+        System.out.println(paneJouer.getId());
+        paneJouer.setVisible(true);
+        panePerso.setVisible(false);
+    }
+
+    @FXML
+    public void initialize() {
+        paneJouer.setVisible(false);
+        panePerso.setVisible(false);
+    }
+
+    @FXML
+    public void CreateMage(){
+        System.out.println(nomPerso.getText());
+
+        //persoCree = new Mage(100,100,1,nomDuPerso.getName());
+    }
+    @FXML
+    public void CreateArcher(){
+        //persoCree = new Archer(100,100,1,nomDuPerso.getName(),5);
+    }
+
+    @FXML
+    public void CreateGuerrier(){
+        //persoCree = new Guerrier(100,100,1,nomDuPerso.getName());
+    }
+
+
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,30 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
+    Stage menu;
+    Stage CreationPerso;
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+
+        menu = primaryStage;
+
+
         primaryStage.setTitle("bjr");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
 
         Guerrier G2 = new Guerrier(100,100,1,"Jj");
@@ -24,10 +36,15 @@ public class Main extends Application {
         archer.AttaqueArme(G2);
 
         System.out.println(G2.getPv());
+
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
 }
